7ff95bea9aa17b56fcf177a7750cceebcf0f8538
gabrieladutra <gabriela_ddutra@outlook.com>
2021-01-04 14:57:49 -0300

    Removed unused Menu class

--- src/main/java/src/Application.java
+++ src/main/java/src/Application.java
@@ -1,11 +1,8 @@
 package src;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.SpringApplication;
+
 
 @SpringBootApplication
 public class Application {
